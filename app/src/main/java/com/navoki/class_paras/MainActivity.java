75743e6a4d58bfbac2511004f389aa16f97d44ca
Shivam <shivamsrivastava.9@gmail.com>
2018-07-18 20:01:48 +0530

    intent

--- app/src/main/java/com/navoki/class_paras/MainActivity.java
+++ app/src/main/java/com/navoki/class_paras/MainActivity.java
@@ -1,13 +1,20 @@
 package com.navoki.class_paras;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +22,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     TextView text1, text2;
     Button button;
+    private EditText email;
+    private EditText pass;
+    private EditText mobile;
+    private EditText pincode;
 
     Context context;
 
@@ -27,6 +38,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         text1 = findViewById(R.id.t1);
         text2 = findViewById(R.id.t2);
         button = findViewById(R.id.button);
+        email = (EditText) findViewById(R.id.email);
+        pass = (EditText) findViewById(R.id.pass);
+        mobile = (EditText) findViewById(R.id.mobile);
+        pincode = (EditText) findViewById(R.id.pincode);
 
         context = MainActivity.this;
 
@@ -41,6 +56,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         button.setOnClickListener(this);
 
+        pincode.setHint("Enter pin code");
+
+        InputFilter[] inputFilters={new InputFilter.LengthFilter(6)};
+        pincode.setFilters(inputFilters);
+
+        pincode.setInputType(InputType.TYPE_CLASS_NUMBER);
+
     }
 
     @Override
@@ -50,5 +72,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         text1.setTextSize(getResources().getDimension(R.dimen.textSize2));
         button.setBackgroundColor(Color.MAGENTA);
         Toast.makeText(context, "Button Click", Toast.LENGTH_SHORT).show();
+
+
+        String strEmail=email.getText().toString();
+        String strMobile=mobile.getText().toString();
+        String strPass=pass.getText().toString();
+        String strPin=pincode.getText().toString();
+
+        Log.e("MainActivity",strEmail);
+        Log.e("MainActivity",strPass+" "+strMobile);
+
+        if(!TextUtils.isEmpty(strEmail)) { // null ""
+
+            if(!TextUtils.isEmpty(strMobile) && strMobile.length()==10){
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtra("email",strEmail);
+                intent.putExtra("mobile",strMobile);
+                intent.putExtra("pass",strPass);
+                intent.putExtra("textColor",Color.BLUE);
+
+                Bundle bundle=new Bundle();
+                bundle.putString("strPin",strPin);
+
+                intent.putExtra("bundle",bundle);
+
+                startActivity(intent);
+                finish();
+            }
+            else
+                Toast.makeText(context, "Enter valid mobile", Toast.LENGTH_SHORT).show();
+        }
+        else
+            Toast.makeText(context, "Enter valid email", Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/com/navoki/class_paras/SecondActivity.java
+++ app/src/main/java/com/navoki/class_paras/SecondActivity.java
@@ -1,14 +1,42 @@
 package com.navoki.class_paras;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 
 public class SecondActivity extends AppCompatActivity {
 
+    private TextView text;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.secondpage);
+
+        text = (TextView) findViewById(R.id.text);
+
+        Intent intent=getIntent();
+        if(intent!=null){
+
+            String email=intent.getStringExtra("email");
+
+            text.setText(email);
+            text.append("\n");
+            text.append(intent.getStringExtra("mobile"));
+
+            Bundle bundle=intent.getBundleExtra("bundle");
+            text.append("\n");
+            text.append(bundle.getString("strPin","N/A"));
+
+            text.setTextColor(intent.getIntExtra("textColor", Color.RED));
+
+
+        }
+
     }
 }
